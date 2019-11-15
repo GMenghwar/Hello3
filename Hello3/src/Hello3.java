3e604dcc81398bd89a193629a06a428210408c33
Gordhan <Gordhan@10.0.0.4>
2019-11-15 13:08:47 +1100

    This is for Hello3

---
+++ Hello3/src/Hello3.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class Hello3 {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "This is one more test");
+	}
+
+}
---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Test for GitHub");
+		System.out.println("After creating Github local respository");
+
+	}
+
+}
